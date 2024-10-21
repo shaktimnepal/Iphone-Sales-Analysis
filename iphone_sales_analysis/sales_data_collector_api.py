c07938f451b6e3c2c368057d3e268f7a17924111
shaktimnepal <shaktimnepal1@gmail.com>
2024-10-20 23:24:34 -0700

    Update sales_data_collector_api.py

--- iphone_sales_analysis/sales_data_collector_api.py
+++ iphone_sales_analysis/sales_data_collector_api.py
@@ -1,1 +1,20 @@
+Python Code:
 
+from pyspark.sql import SparkSession
+
+# Initialize Spark session
+spark = SparkSession.builder.appName("Sales Data Collector").enableHiveSupport().getOrCreate()
+
+
+def sales_data_collector_api(spark, text_file_path):
+    # Load the sales data from the given text file with '|' delimiter
+    sales_df = spark.read.option("header", True).option("delimiter", "|").csv(text_file_path)
+
+    # Write the data to a Hive table with partitioning on 'sale_date'
+    sales_df.write.mode("overwrite").partitionBy("sale_date").format("parquet").saveAsTable("iphone_sales_analysis.sales_hive_table")
+
+    return "iphone_sales_analysis.sales_hive_table"
+
+if __name__ == '__main__':
+    filepath = 'file:///home/takeo/data/iphone_sales_analysis_project/sales_data'
+    sales_data_collector_api(spark, filepath)
