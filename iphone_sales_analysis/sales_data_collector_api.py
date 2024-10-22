10c15d9b48364c466874b717b0bc8dc32b509c63
shaktimnepal <shaktimnepal1@gmail.com>
2024-10-22 13:25:00 -0700

    Update sales_data_collector_api.py

--- iphone_sales_analysis/sales_data_collector_api.py
+++ iphone_sales_analysis/sales_data_collector_api.py
@@ -9,11 +9,13 @@ spark = SparkSession.builder.appName("Sales Data Collector").enableHiveSupport()
 def sales_data_collector_api(spark, text_file_path):
     # Load the sales data from the given text file with '|' delimiter
     sales_df = spark.read.option("header", True).option("delimiter", "|").csv(text_file_path)
+    spark.conf.set("hive.exec.dynamic.partition", "true")
+    spark.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
 
     # Write the data to a Hive table with partitioning on 'sale_date'
-    sales_df.write.mode("overwrite").partitionBy("sale_date").format("parquet").saveAsTable("iphone_sales_analysis.sales_hive_table")
+    sales_df.write.mode("overwrite").partitionBy("sale_date").format("parquet").saveAsTable("iphone_sales_analysis1.sales_hive_table")
 
-    return "iphone_sales_analysis.sales_hive_table"
+    return "iphone_sales_analysis1.sales_hive_table"
 
 if __name__ == '__main__':
     filepath = 'file:///home/takeo/data/iphone_sales_analysis_project/sales_data'
